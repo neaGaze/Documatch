55075a3af94f2757a627cb7c0bc84ff209bf2a7c
Nigesh Shakya <nizeshshakya@gmail.com>
2016-02-10 20:08:27 -0600

    tf-idf calculated but not sure about output

--- P1_NIGESH.py
+++ P1_NIGESH.py
@@ -1,4 +1,5 @@
 import os
+import math
 import re
 import nltk
 from nltk.corpus import stopwords
@@ -10,13 +11,27 @@ tokenized_data = []
 stemmed_data = []
 count_datafiles = 0
 
+def cosine_similarity(v1,v2):
+    "compute cosine similarity of v1 to v2: (v1 dot v2)/{||v1||*||v2||)"
+    sumxx, sumxy, sumyy = 0, 0, 0
+    for i in range(len(v1)):
+        x = v1[i]; y = v2[i]
+        sumxx += x*x
+        sumyy += y*y
+        sumxy += x*y
+    return sumxy/math.sqrt(sumxx*sumyy)
+
+def calculate_tdidf(vec):
+    tfidf_vec = {}
+    return tfidf_vec
+
 # function to separate words from sentences
 def word_separator(row):
     onlyApha = re.sub(r'[^a-zA-Z]+', " ", row)
     tokens = onlyApha.split()
     return tokens
 
-# function to tokenize the words
+# NOT USED. function to tokenize the words
 def tokenize(row):
     sentences = []
     tokenizer_plan = nltk.data.load('tokenizers/punkt/english.pickle')
@@ -41,19 +56,60 @@ count = 0
 for entry in raw_data:
    # tokenizer = RegexpTokenizer(r'[^a-zA-Z]')
     if count != 11 and count != 13 and count != 14 and count < 15:
-        tokenized_data += word_separator(entry)
+        tokenized_data.append(word_separator(entry))
         count_datafiles += 1
     count += 1
 
 # stemming process
 stemmer = PorterStemmer()
-for each in tokenized_data:
-    if len(each) > 0:
-        print each
-        stemmed_data.append(stemmer.stem(each))
+for innerList in tokenized_data:
+    tmp_list = []
+    for each in innerList:
+        if len(each) > 0:
+            print each
+            tmp_list.append(stemmer.stem(each))
+    stemmed_data.append(tmp_list)
 
+# removing the stopwords
 stepword = stopwords.words('english')
-words_not_common = [each for each in stemmed_data if each not in stepword]
+words_not_common = []
+tfidf_vec_list = []
+for a in stemmed_data:
+    words_not_common.append([each for each in a if each not in stepword])
+    #tfidf_vec_list.append(calculate_tdidf(words_not_common[a]))  
+
+# calculate the tdidf vector
+idf = {}
+tf_list = []
+for list_terms in words_not_common:
+    tf = {} 
+    for term in list_terms:
+        flag = True
+
+        if term in tf:
+            tf[term] += 1
+        else:
+            tf[term] = 1
+
+ #   for term in list_terms:
+        if term in idf and flag:
+            idf[term] += 1
+            #break
+            flag = False
+        else:
+            idf[term] = 1
+
+    tf_list.append(tf)
+
+cnt = 0
+for list_terms in words_not_common:
+    tfidf = {}
+    for term in list_terms:
+        first_part = 1 + math.log10(tf_list[cnt][term])
+        second_part = math.log10(float(len(words_not_common)) / float(idf[term]))
+        tfidf[term] = first_part * second_part
+    tfidf_vec_list.append(tfidf)
+    cnt += 1
 
 
 
