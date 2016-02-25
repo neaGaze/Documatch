be4975c86404b3919fde87ddd9949e50f17ad76c
Nigesh Shakya <nizeshshakya@gmail.com>
2016-02-24 23:58:46 -0600

    Completed querydocsim :D

--- P1_NIGESH_2.py
+++ P1_NIGESH_2.py
@@ -16,23 +16,39 @@ TOTAL_DOCUMENTS_SCANNED = 0
 list_val = []
 
 # return the cosine similairty between a query string and a document
-def querydocsim(qstring,filename):
-    return False
+def querydocsim(tfidf, qstring,filename):
+    processed_token = token_processor(qstring.lower())
+    doc = tfidf[filename]
+    tf_qstring = {}
+    weighted_avg_mean = 0
+    
+    for token in processed_token:
+        if token not in tf_qstring:
+            count = processed_token.count(token)
+            tf_qstring[token] = ((1.0 + float(math.log10(float(count)))) if count != 0 else 0)
+            weighted_avg_mean += tf_qstring[token]**(2)
+
+    cosine_simil = 0
+    
+    for tmp_key in tf_qstring:
+        tf_qstring[tmp_key] = ((tf_qstring[tmp_key])) / weighted_avg_mean**(1/2)
+        for tmp_key2 in doc:
+            if tmp_key == tmp_key2:
+                cosine_simil += tf_qstring[tmp_key] * doc[tmp_key2]
+
+    return cosine_simil
 
 # return the cosine similarity betwen two speeches (files)
 def docdocsim(tfidf, filename1,filename2):
     doc1 = tfidf[filename1]
     doc2 = tfidf[filename2]
-    cosine_sim = []
+    cosine_sum = 0
 
     for key in doc1:
         for each in doc2:
             if key == each:
-                cosine_sim.append(doc1[key] * doc2[each])
-        # cosine_sim += [doc2[each]*doc1[key] for each in doc2 if key in doc2]
+                cosine_sum += doc1[key] * doc2[each]
 
-    print('cosine list: ', len(cosine_sim))
-    cosine_sum = sum(cosine_sim)
     return cosine_sum
 
 # return the inverse document frequency of a token. If the token doesn't exist in the corpus, return 0
@@ -54,16 +70,6 @@ def getcount(token):
 def query(qstring):
     processed_token = token_processor(qstring.lower())
     return False
-
-def cosine_similarity(v1,v2):
-    "compute cosine similarity of v1 to v2: (v1 dot v2)/{||v1||*||v2||)"
-    sumxx, sumxy, sumyy = 0, 0, 0
-    for i in range(len(v1)):
-        x = v1[i]; y = v2[i]
-        sumxx += x*x
-        sumyy += y*y
-        sumxy += x*y
-    return sumxy/math.sqrt(sumxx*sumyy)
 
 def calc_tf_idf(corpus_token_repo):
     for doc in tokens_in_all_doc:
@@ -128,7 +134,8 @@ def read_saved_data(filename):
     with open(os.path.join('./gen', filename)) as da:
         str_data += da.read()
 
-    return ast.literal_eval(str_data)
+    tfidf = ast.literal_eval(str_data)
+    return tfidf
 
 tbl = read_file_tokenize()
 #temp_list = [compute_tf_idf(each) for each in tokens_in_all_doc['2012-10-22.txt'][0:25]]
